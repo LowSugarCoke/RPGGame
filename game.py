fddbeadb0a592947117f593c722c4068a0a8af95
Jack Lee <jack20499@gmail.com>
2022-09-09 23:39:22 +0800

    feat(initial): Initial the pygame
    *Since I want to do a RGB game be students rewards after completing
    homework. Start to write RPG game and have fun!

--- game.py
+++ game.py
@@ -2,9 +2,20 @@ import pygame
 
 
 class RPGGame:
-    def __init__(self, w=1920, h=1080):
+    def __init__(self, w=800, h=600):
         self.w = w
         self.h = h
         # init display
         self.display = pygame.display.set_mode((self.w, self.h))
         pygame.display.set_caption('RPG Game')
+        clock = pygame.time.Clock()
+        crashed = False
+        while not crashed:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    crashed = True
+                    print(event)
+                    pygame.display.update()
+                    clock.tick(60)
+                    pygame.quit()
+                    quit()
