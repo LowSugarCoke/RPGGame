539f81658d52282343d197ba29b2d7a1c7bf0efb
Jack Lee <jack20499@gmail.com>
2022-09-10 01:09:33 +0800

    feat($monster): initial monster
    *Make a monster quickly for testing how to import gif into the pygame. Find
    out that pygame is not supporting animation gif file. It just only show
    static image

--- game.py
+++ game.py
@@ -1,16 +1,23 @@
 import pygame
+from monster import Monster
 
 
 class RPGGame:
     def __init__(self, w=800, h=600):
+        pygame.init()
         self.w = w
         self.h = h
+
         # init display
-        self.display = pygame.display.set_mode((self.w, self.h))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         pygame.display.set_caption('RPG Game')
         clock = pygame.time.Clock()
         crashed = False
+
+        self.monster = Monster(self)
         while not crashed:
+            pygame.display.flip()
+            self.monster.blitme()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     crashed = True
