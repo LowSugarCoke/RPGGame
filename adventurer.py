6419bdb5a8556d1defd8e988feed0a9444920fbe
Jack Lee <jack20499@gmail.com>
2022-09-10 15:45:43 +0800

    feat($adventurer): initial adventurer by adding adventurer
    *Add swordsman.png by cut one of character in adventurer.png. And modify
    the monster and swordsman position

--- adventurer.py
+++ adventurer.py
@@ -2,15 +2,14 @@ import pygame
 from pygame.sprite import Sprite
 
 
-class Monster(Sprite):
+class Adventurer(Sprite):
     def __init__(self, game):
         super().__init__()
         self.screen = game.screen
-        self.image = pygame.image.load('img/monster.gif')
+        self.image = pygame.image.load('img/swordsman.png')
         self.rect = self.image.get_rect()
-
         self.rect.x = self.screen.get_width()/2 - self.rect.width/2
-        self.rect.y = 0
+        self.rect.y = self.screen.get_height() - self.rect.height
         self.x = float(self.rect.x)
 
     def blitme(self):
