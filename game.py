b6d822a3ee61b837660f2ea103feb2c22e20806c
Jack Lee <Jack.cy.Lee@viewsonic.com>
2022-09-27 16:47:07 +0800

    feat(harm): add damage effect

--- game.py
+++ game.py
@@ -6,7 +6,6 @@ from attack import Attack
 from monster_attack import MonsterAttack
 
 
-
 class RPGGame:
     def __init__(self, w=800, h=600):
         pygame.init()
@@ -24,7 +23,7 @@ class RPGGame:
         #init music
         pygame.mixer.init() # add this line
         pygame.mixer.music.load(os.path.join("Sound", 'battle.ogg'))
-        pygame.mixer.music.play(-1)
+        # pygame.mixer.music.play(-1)
 
         
         self.monster = Monster(self)
@@ -37,14 +36,20 @@ class RPGGame:
         self.attack = Attack(self, a_x, a_y)
 
         i = 0
+        adventurerDamage=0
+        monsterDamage = 0
         while not crashed:            
             clock.tick(10)            
             self.screen.fill((0,0,0))
 
             self.attack.blitme(i)
             self.adventurer.blitme()
+            adventurerDamage = self.adventurer.showHarm(adventurerDamage)
+                            
             self.monster.blitme()
-            self.monster_attack.blitme(i)
+            self.monster_attack.blitme(i)           
+            monsterDamage = self.monster.showHarm(monsterDamage)
+
             i = i+1
             if(i == 12):
                 self.monster_attack.randomPosition()
@@ -54,6 +59,7 @@ class RPGGame:
                 if crash_result == 1:         
                     damage = self.attack.damage
                     self.monster.life -= damage
+                    monsterDamage = damage
                 else:
                     self.adventurer.move()
                     px, py = self.adventurer.getPosition()
@@ -64,6 +70,8 @@ class RPGGame:
                     print("Harm ")
                     damage = self.monster_attack.damage
                     self.adventurer.life -= damage
+                    adventurerDamage = damage
+
                                         
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
--- monster.py
+++ monster.py
@@ -6,6 +6,7 @@ class Monster(Sprite):
     def __init__(self, game):
         super().__init__()
         self.screen = game.screen
+        self.font = pygame.font.SysFont("arial", 36)
         self.image = pygame.image.load('img/monster.gif')
         self.image = pygame.transform.rotozoom(self.image, 0, 0.5)
         self.rect = self.image.get_rect()
@@ -14,6 +15,7 @@ class Monster(Sprite):
         self.blood_bar_position = [self.screen.get_width()/4, self.rect.y]
         self.life = self.screen.get_width()/2
         self.damage = 100
+        self.damageCount = 0
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
@@ -27,3 +29,16 @@ class Monster(Sprite):
 
     def getMonsterRect(self):
         return self.rect
+
+    def showHarm(self, harm):
+        if harm!=0 :
+            textSurface = self.font.render(str(harm), True, (255,0,0), (0, 0, 0))
+            self.screen.blit(textSurface,(self.rect.x+200,self.rect.y+30))
+            self.damageCount+=1
+                            
+        if self.damageCount > 6:
+            harm = 0
+            self.damageCount = 0
+            return 0
+
+        return harm
