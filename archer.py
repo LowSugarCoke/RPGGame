105face124d70b4e55357cfab65ad0c2b8c677c2
Jack Lee <Jack.cy.Lee@viewsonic.com>
2022-09-28 11:11:42 +0800

    feat($archer): add new character archer
    *Rename folder sword to sword_attack
    
    *Replace the attack file from background is black to transparent
    
    *Add archer

--- archer.py
+++ archer.py
@@ -2,22 +2,20 @@ import pygame
 from pygame.sprite import Sprite
 
 
-class Adventurer(Sprite):
+class Archer(Sprite):
     def __init__(self, game):
         super().__init__()
         self.screen = game.screen
         self.font = pygame.font.SysFont("arial", 36)
-        self.image = pygame.image.load('img/swordsman.png')
-        # self.image = pygame.transform.rotozoom(self.image, 0, 0.5)
+        self.image = pygame.image.load('img/archer.png')
         self.rect = self.image.get_rect()
-        self.rect.x = self.screen.get_width()*3/7 - self.rect.width/2
+        self.rect.x = self.screen.get_width()*1/5 - self.rect.width/2
         self.rect.y = self.screen.get_height()-self.rect.height
         self.blood_bar_position = [self.rect.x, self.rect.y]
         self.life = 1000
         self.damageCount = 0
 
     def blitme(self):
-        
         self.screen.blit(self.image, self.rect)
         pygame.draw.rect(self.screen, (0, 128, 0),
                          (self.blood_bar_position[0], self.blood_bar_position[1], self.rect.width, 8))
