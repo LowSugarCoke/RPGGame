dac843e821390b22b4dbdb911de44165546a03ea
Jack Lee <Jack.cy.Lee@viewsonic.com>
2022-09-29 23:17:24 +0800

    feat(magician): add new character magician

--- adventurer_data.py
+++ adventurer_data.py
@@ -12,10 +12,10 @@ class AdventurerData:
         self.rect.y = 0
         self.blood_bar_position = [self.rect.x, self.rect.y]
         self.life = 0
-        self.damageCountDistance = [0,100]        
         self.attack = None
         self.attackPosition = []
         self.moveSpeed = 0
+        self.cdTime = 0
 
     def createSwordsman(self, swordsManAttack):
         self.font = pygame.font.SysFont("arial", 36)
@@ -30,11 +30,12 @@ class AdventurerData:
         self.attack = swordsManAttack
         self.attack.image = pygame.image.load('img/sword_attack/attack-0.png')
         self.attack.rect = self.attack.image.get_rect()
-        self.attack.damage = 100
+        self.attack.damageCountDistance = [10,50]         
         self.attack.rotation = -90
         self.attack.zoom = 0.4
         self.attack.loadImages('img/sword_attack/attack-')
         self.distanceWithMonster = 250
+        self.cdTime = 1
    
 
     def createArcher(self, archerAttack):
@@ -50,7 +51,7 @@ class AdventurerData:
         self.attack = archerAttack
         self.attack.image = pygame.image.load('img/sword_attack/attack-0.png')
         self.attack.rect = self.attack.image.get_rect()
-        self.attack.damage = 100
+        self.attack.damageCountDistance = [20,70]         
         self.attack.rotation = -130
         self.attack.zoom = 0.5
         self.attack.loadImages('img/archer_attack/attack-')
@@ -64,14 +65,35 @@ class AdventurerData:
         self.rect.y = self.height-self.rect.height
         self.blood_bar_position = [self.rect.x, self.rect.y]
         self.life = 1000
-        self.moveSpeed = 5
+        self.moveSpeed = 10
         self.attackPosition = [-30, -140]
         self.attack = orcAttack
         self.attack.image = pygame.image.load('img/orc_attack/attack-0.png')
         self.attack.rect = self.attack.image.get_rect()
-        self.attack.damage = 100
+        self.attack.damageCountDistance = [0,100]      
         self.attack.rotation = -90
         self.attack.zoom = 0.4
         self.attack.loadImages('img/orc_attack/attack-')
         self.distanceWithMonster = 300
+        self.cdTime = 2
+    
+    def createMagician(self, magicianAttack):
+        self.font = pygame.font.SysFont("arial", 36)
+        self.image = pygame.image.load('img/magician.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = self.width*5/7 - self.rect.width/2
+        self.rect.y = self.height-self.rect.height
+        self.blood_bar_position = [self.rect.x, self.rect.y]
+        self.life = 1000
+        self.moveSpeed = 5
+        self.attackPosition = [-220, -400]
+        self.attack = magicianAttack
+        self.attack.image = pygame.image.load('img/magician_attack/attack-0.png')
+        self.attack.rect = self.attack.image.get_rect()
+        self.attack.damageCountDistance = [30,150]      
+        self.attack.rotation = 0
+        self.attack.zoom = 0.5
+        self.attack.loadImages('img/magician_attack/attack-')
+        self.distanceWithMonster = 500
+        self.cdTime = 5
  
