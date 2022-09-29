a3a6fb11711f61aec9f23c383c08db4f93798981
Jack Lee <Jack.cy.Lee@viewsonic.com>
2022-09-30 00:41:52 +0800

    fix($archer): change archer img

--- adventurer.py
+++ adventurer.py
@@ -33,9 +33,9 @@ class Adventurer(Sprite):
     def blitme(self):        
         self.screen.blit(self.image, self.rect)
         pygame.draw.rect(self.screen, (0, 128, 0),
-                         (self.blood_bar_position[0], self.blood_bar_position[1], self.rect.width, 8))
+                         (self.blood_bar_position[0], self.blood_bar_position[1], 80, 8))
         pygame.draw.rect(self.screen, (255, 0, 0),
-                         (self.blood_bar_position[0], self.blood_bar_position[1], (self.life * self.rect.width/self.maxLife), 8))
+                         (self.blood_bar_position[0], self.blood_bar_position[1], (self.life * 80/self.maxLife), 8))
 
     def getPosition(self):
         return self.rect.x, self.rect.y
