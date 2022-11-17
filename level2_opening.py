23402e8eefb674451435d04fa2d2dc924bbdfce6
Jack Lee <Jack.cy.Lee@viewsonic.com>
2022-11-17 15:32:10 +0800

    feat(level2_opening): add level 2 opening

--- level2_opening.py
+++ level2_opening.py
@@ -8,7 +8,7 @@ blue = (0, 0, 128)
 black = (0,0,0)
 
 
-class Level1Opening():
+class Level2Opening():
     def __init__(self, game):
         self.screen = game.screen
         self.font = pygame.font.Font('freesansbold.ttf', 16)
@@ -19,18 +19,18 @@ class Level1Opening():
         distance = 100
         # content
         font = pygame.font.Font('freesansbold.ttf',30)
-        content = 'Level 1'
+        content = 'Level 2'
         text = font.render(content, True, black, white)
         textRect = text.get_rect()
         textRect.center = (self.screen.get_width()/2 , self.screen.get_height()*1/2-150)
         self.screen.blit(text, textRect)
 
         # Swords man
-        image = pygame.image.load('./Img/ghost.png')
-        image = pygame.transform.rotozoom(image, 0,3)
+        image = pygame.image.load('./Img/monster.gif')
+        # image = pygame.transform.rotozoom(image, 0,3)
         self.swordsmanPosition = image.get_rect()
-        self.swordsmanPosition.x =  self.screen.get_width()/2 -100
-        self.swordsmanPosition.y =  self.screen.get_height()/2-150
+        self.swordsmanPosition.x =  self.screen.get_width()/2 -130
+        self.swordsmanPosition.y =  self.screen.get_height()/2-130
         self.screen.blit(image, self.swordsmanPosition)
 
 
